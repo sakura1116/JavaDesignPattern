68d6ae6fbf861440615b41ecc0c646b3dd0dc14e
sakura1116 <daisuke17260@gmail.com>
2018-11-29 00:39:27 +0900

    iterator

---
+++ src/iterator/Main.java
@@ -0,0 +1,18 @@
+package iterator;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// BookShelf bs = new BookShelf(4);
+		BookShelf bs = new BookShelf();
+		bs.appendBook(new Book("book A"));
+		bs.appendBook(new Book("Book B"));
+		bs.appendBook(new Book("Book C"));
+		bs.appendBook(new Book("Book D"));
+		Iterator it = bs.iterator();
+		while (it.hasNext()) {
+			Book book = (Book) it.next();
+			System.out.println("" + book.getName());
+		}
+	}
+}
