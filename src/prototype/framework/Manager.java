7afd7788cc897076836adfe711e8e11a798dc2d9
sakura1116 <daisuke17260@gmail.com>
2019-01-05 21:09:18 +0900

    prototype

---
+++ src/prototype/framework/Manager.java
@@ -0,0 +1,17 @@
+package prototype.framework;
+
+import java.util.Hashtable;
+
+public class Manager {
+	private Hashtable<String, Product> showcase = new Hashtable<String, Product>();
+
+	public void register(String name, Product proto) {
+		showcase.put(name, proto);
+	}
+
+	public Product create(String protoname) {
+		Product product = (Product) showcase.get(protoname);
+		return product.createClone();
+	}
+
+}
